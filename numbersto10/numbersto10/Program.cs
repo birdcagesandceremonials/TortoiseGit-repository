f3eaed3d029548879e5f61e4eb395e1f46eb2f4c
birdcagesandceremonials <zendaya12@abv.bg>
2016-09-21 22:45:41 -0700

    Awful changes.

--- numbersto10/numbersto10/Program.cs
+++ numbersto10/numbersto10/Program.cs
@@ -16,50 +16,7 @@ namespace numbersto10
 			
 			
 			
-			
-			//I definitely love Florence Welch and Sarah Paulson and this is one of my 99 problems along with learning programming. 
-			//It's totally cool tho.
-			
-                else
-                {
-                    if (num == 3)
-                    {
-                        Console.WriteLine("three");
-                    }
-                    else
-                    {
-                        if (num == 4)
-                        {
-                            Console.WriteLine("four");
-                        }
-                        else
-                        {
-                            if (num == 5)
-                            {
-                                Console.WriteLine("five");
-                            }
-                            else
-                            {
-                                if (num == 6)
-                                {
-                                    Console.WriteLine("six");
-                                }
-                                else
-                                {
-                                    if (num == 7)
-                                    {
-                                        Console.WriteLine("seven");
-                                    }
-                                    else
-                                    {
-										//It is a wonderful day to do some programming. Yep :) ^ 
-                                        if (num == 8)
-                                        {
-                                            Console.WriteLine("eight");
-                                        }
-                                    }
-                                }
-                            }
+
                         }
                     }
                         
